c6230ef515406d337f0c5f233f3a8a645f33c8d2
Ivan Cherniy <paalomnik@gmail.com>
2018-01-23 22:07:26 +0200

    added check alive endpoint

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
-from flask import Flask
+from flask import Flask, jsonify
 import requests
 
 app = Flask(__name__)
 app.config.from_pyfile('settings.py')
 
-requests.post(
-    'http://{master_node}/register'.format(master_node=app.config['MASTER_NODE_ADDRESS']),
-    data={
-        'name': app.config['IDENTITY_FOR_SERVER'],
-        'secret': app.config['SECRET_TOKEN'],
-        'command_ip': '{host}:{port}'.format(host=app.config['HOST'], port=app.config['PORT'])
-    }
-)
+try:
+    requests.post(
+        'http://{master_node}/register'.format(master_node=app.config['MASTER_NODE_ADDRESS']),
+        data={
+            'name': app.config['IDENTITY_FOR_SERVER'],
+            'secret': app.config['SECRET_TOKEN'],
+            'command_ip': '{host}:{port}'.format(host=app.config['HOST'], port=app.config['PORT'])
+        }
+    )
+except:
+    pass
 
 
 @app.route('/command/<command>', defaults={'command': None})
 def command(command):
     pass
+
+
+@app.route('/check-alive')
+def check_alive():
+    return jsonify({'alive': 'yes'})
 
 
 if __name__ == '__main__':
