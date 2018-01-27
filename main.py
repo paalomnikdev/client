ee75d67872e51f4780910c678f9f3d9b37db3e88
Ivan Cherniy <paalomnik@gmail.com>
2018-01-27 18:20:10 +0200

    added loop to register

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 from flask import Flask, jsonify
 import requests
+import time
 
 app = Flask(__name__)
 app.config.from_pyfile('settings.py')
 
-try:
-    requests.post(
-        'http://{master_node}/register'.format(master_node=app.config['MASTER_NODE_ADDRESS']),
-        data={
-            'name': app.config['IDENTITY_FOR_SERVER'],
-            'secret': app.config['SECRET_TOKEN'],
-            'command_ip': '{host}:{port}'.format(host=app.config['HOST'], port=app.config['PORT'])
-        }
-    )
-except:
-    pass
+
+for x in range(0, 10):
+    try:
+        requests.post(
+            'http://{master_node}/register'.format(master_node=app.config['MASTER_NODE_ADDRESS']),
+            data={
+                'name': app.config['IDENTITY_FOR_SERVER'],
+                'secret': app.config['SECRET_TOKEN'],
+                'command_ip': '{host}:{port}'.format(host=app.config['HOST'], port=app.config['PORT'])
+            }
+        )
+        break
+    except:
+        time.sleep(20)
+        continue
 
 
 @app.route('/command/<command>', defaults={'command': None})
